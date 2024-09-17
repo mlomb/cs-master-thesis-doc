5fa2d375700cd6ccaa3674d41a9076fa67c3dd29
Martín Lombardo <to@mlomb.me>
2024-09-17 04:18:58 -0300

    mean line

--- thesis/dynamic/generate.py
+++ thesis/dynamic/generate.py
@@ -117,11 +117,17 @@ def exp_1_baseline():
 def quantization_error():
     df = pd.read_csv('../../assets/results/quant_errors.csv')
     df["err"] = (df["expected_output"] - df["output"])
+    df["rel_err"] = (df["err"] / df["expected_output"]).abs()
 
     sns.displot(df, x='err', height=3, aspect=1.3, bins=35) # , linewidth=0
     plt.xlabel('Error of quantized model')
     plt.ylabel('Count')
-    plt.xlim(-100, 100)
+
+    # draw quantile 0.5 line
+    plt.axvline(x=df["err"].mean(), color='r', linestyle='dotted')
+    plt.text(df["err"].mean(), 8400, 'mean', color='r', ha='left', va='top', rotation=90)
+    print(f"Quantization error: min: {df["output"].min()} max: {df["output"].max()} mean: {df["output"].mean()}")
+
     #plt.ylim(0, 3000)
     plt.savefig("./output/quant_errors.pdf", format='pdf')
     plt.close()
