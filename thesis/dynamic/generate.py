d09b8f794b24a3f8ce28cd1e2d2b2616e8e96165
Martín Lombardo <to@mlomb.me>
2024-09-19 18:25:49 -0300

    training source dataset

--- thesis/dynamic/generate.py
+++ thesis/dynamic/generate.py
@@ -119,13 +119,13 @@ def quantization_error():
     df["err"] = (df["expected_output"] - df["output"])
     df["rel_err"] = (df["err"] / df["expected_output"]).abs()
 
-    sns.displot(df, x='err', height=3, aspect=1.3, bins=35) # , linewidth=0
+    sns.displot(df, x='err', height=3, aspect=1.3, bins=20) # , linewidth=0
     plt.xlabel('Error of quantized model')
     plt.ylabel('Count')
 
     # draw quantile 0.5 line
     plt.axvline(x=df["err"].mean(), color='r', linestyle='dotted')
-    plt.text(df["err"].mean(), 8400, 'mean', color='r', ha='left', va='top', rotation=90)
+    plt.text(df["err"].mean(), 11500, 'mean', color='r', ha='left', va='top', rotation=90)
     print(f"Quantization error: min: {df["output"].min()} max: {df["output"].max()} mean: {df["output"].mean()}")
 
     #plt.ylim(0, 3000)
