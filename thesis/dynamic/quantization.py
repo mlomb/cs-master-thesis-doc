46cea7cd66c44c04ab663addc831bb654dd345b9
Martín Lombardo <to@mlomb.me>
2024-10-22 14:20:13 -0300

    new quantized figure

--- thesis/dynamic/quantization.py
+++ thesis/dynamic/quantization.py
@@ -5,23 +5,38 @@ import matplotlib.pyplot as plt
 
 # use latex for font rendering
 matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams['font.family'] = ['serif']
 
 
 def quantization_error():
     df = pd.read_csv('../../assets/results/quant_errors.csv')
-    df["err"] = (df["expected_output"] - df["output"])
-    df["rel_err"] = (df["err"] / df["expected_output"]).abs()
+    df = df.sample(n=100000)
 
-    sns.displot(df, x='err', height=3, aspect=1.3, bins=20) # , linewidth=0
-    plt.xlabel('Error of quantized model')
+    df["diff"] = df["expected_output"] - df["output"]
+
+    fig, (ax1,ax2) = plt.subplots(1,2, figsize=(8, 3))
+    sns.kdeplot(df['output'], ax=ax1, bw_adjust=0.2, legend="Quantized model")
+    sns.kdeplot(df['expected_output'], ax=ax1, bw_adjust=0.2, legend="Float model")
+
+    ax1.set_xlabel('Evaluation')
+    ax1.set_ylabel('Density (log)')
+    ax1.set_yscale('log')
+    #ax.set_xlim(-1000, 1000)
+    #ax.set_xlim(-500, 500)
+    ax1.set_ylim(1e-5, 1e-2)
+    ax1.legend(labels=['Quantized model', 'Float model'])
+    ax1.set_title('Evaluation distribution')
+
+    sns.histplot(df, x='diff',bins=40, ax=ax2) # , linewidth=0
+    plt.xlabel('Error (float - quantized)')
     plt.ylabel('Count')
+    plt.title('Error distribution')
 
     # draw quantile 0.5 line
-    plt.axvline(x=df["err"].mean(), color='r', linestyle='dotted')
-    plt.text(df["err"].mean(), 11500, 'mean', color='r', ha='left', va='top', rotation=90)
-    print(f"Quantization error: min: {df["output"].min()} max: {df["output"].max()} mean: {df["output"].mean()}")
+    plt.axvline(x=df["diff"].mean(), color='r', linestyle='dotted')
+    plt.text(df["diff"].mean(), 20000, 'mean', color='r', ha='left', va='top', rotation=90)
 
-    #plt.ylim(0, 3000)
+    plt.tight_layout()
     plt.savefig("./output/quant_errors.pdf", format='pdf')
     plt.close()
 
