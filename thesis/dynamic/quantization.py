fe915b11e32b9189f7ba2cb40373796cdd0b0c5e
Martín Lombardo <to@mlomb.me>
2024-10-22 15:49:57 -0300

    quantization errors

--- thesis/dynamic/quantization.py
+++ thesis/dynamic/quantization.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -14,6 +15,11 @@ def quantization_error():
 
     df["diff"] = df["expected_output"] - df["output"]
 
+    def to_wdl(x):
+        return 1/(1 + np.exp(-x / 361))
+
+    df["diff"] = to_wdl(df["expected_output"]) - to_wdl(df["output"])
+
     fig, (ax1,ax2) = plt.subplots(1,2, figsize=(8, 3))
     sns.kdeplot(df['output'], ax=ax1, bw_adjust=0.2, legend="Quantized model")
     sns.kdeplot(df['expected_output'], ax=ax1, bw_adjust=0.2, legend="Float model")
@@ -27,14 +33,14 @@ def quantization_error():
     ax1.legend(labels=['Quantized model', 'Float model'])
     ax1.set_title('Evaluation distribution')
 
-    sns.histplot(df, x='diff',bins=40, ax=ax2) # , linewidth=0
-    plt.xlabel('Error (float - quantized)')
+    sns.histplot(df, x='diff',bins=60, ax=ax2) # , linewidth=0
+    plt.xlabel('Error in WDL (float - quantized)')
     plt.ylabel('Count')
-    plt.title('Error distribution')
+    plt.title('Error distribution in WDL')
 
     # draw quantile 0.5 line
     plt.axvline(x=df["diff"].mean(), color='r', linestyle='dotted')
-    plt.text(df["diff"].mean(), 20000, 'mean', color='r', ha='left', va='top', rotation=90)
+    plt.text(df["diff"].mean(), 18000, 'mean', color='r', ha='left', va='top', rotation=90)
 
     plt.tight_layout()
     plt.savefig("./output/quant_errors.pdf", format='pdf')
